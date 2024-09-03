5744d10af41c212f5085e3e4f0f977744887dd0a
Peter Jones <peter.jones@loyaltylogistix.com>
2024-09-03 15:10:23 +0100

    add library and console projects

---
+++ src/HtmlToImage.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using HtmlToImage.Library;
+
+
+string startupPath = AppContext.BaseDirectory;
+var filePath = Path.Combine(startupPath, "../../../../../_SolutionItems/");
+
+string sampleFilePath = Path.Combine(filePath, "SampleHtmlFile/Sample1.html");
+string fileContent = await File.ReadAllTextAsync(sampleFilePath);
+
+var outputPath = Path.Combine(filePath, $"ThumbnailImages/{Guid.NewGuid()}.png");
+await ImageGenerator.GenerateImageAsync(fileContent, 400, 300, outputPath);
---
+++ src/HtmlToImage.Library/ImageGenerator.cs
@@ -0,0 +1,38 @@
+﻿using PuppeteerSharp;
+
+namespace HtmlToImage.Library;
+public static class ImageGenerator
+{
+    /// <summary>
+    /// You may need to install additional dependencies for Chromium to run properly on Linux. For example, you might need
+    /// to install libglib2.0-0, libnss3, libnspr4, libatk1.0-0, libatk-bridge2.0-0, libcups2, libdrm2, libdbus-1-3, libxkbcommon0, 
+    /// libxcomposite1, libxdamage1, libxrandr2, libgbm1, libasound2, and other related libraries. 
+    /// These can typically be installed using your distribution's package manager.
+    ///
+    /// If you're running Puppeteer-Sharp in a Docker container on Linux, you'll need to ensure your Docker image includes these dependencies.
+    /// </summary>
+
+    public static async Task GenerateImageAsync(string html, int width, int height, string targetPath)
+    {
+        var browserFetcher = new BrowserFetcher();
+
+        /* Will download the latest version of Chromium for the current platform if not already installed */
+        await browserFetcher.DownloadAsync();
+
+        await using var browser = await Puppeteer.LaunchAsync(
+            new LaunchOptions { Headless = true });
+
+        await using var page = await browser.NewPageAsync();
+
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            Width = width,
+            Height = height
+        });
+
+        await page.SetContentAsync(html);
+
+        await page.ScreenshotAsync(targetPath);
+        await browser.CloseAsync();
+    }
+}
